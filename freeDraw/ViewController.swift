42b015da4bc0654ba1c92d417d264d257099f53c
xuthief <xuthief@gmail.com>
2016-06-14 17:54:16 +0800

    Initial Commit

---
+++ freeDraw/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  freeDraw
+//
+//  Created by xuthief on 16/6/14.
+//  Copyright © 2016年 xuthief. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
