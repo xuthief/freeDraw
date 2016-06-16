81fe85a868c1eefa384c4c1788600fc82c8a4f62
xuthief <xuthief@gmail.com>
2016-06-16 11:40:07 +0800

    init version of free draw

---
+++ freeDraw/LinerInterpView.swift
@@ -0,0 +1,55 @@
+//
+//  LinerInterpView.swift
+//  freeDraw
+//
+//  Created by xuthief on 16/6/14.
+//  Copyright © 2016年 xuthief. All rights reserved.
+//
+
+import UIKit
+
+class LinerInterpView: UIView {
+
+    var path : UIBezierPath? = nil;
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.multipleTouchEnabled = true
+        self.backgroundColor = UIColor.whiteColor()
+        path = UIBezierPath.init()
+        path?.lineWidth = 2;
+    }
+    
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+        UIColor.blackColor().setStroke()
+        path?.stroke()
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first!
+        let p = touch.locationInView(self)
+        path?.moveToPoint(p)
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        let touch = touches.first!
+        let p = touch.locationInView(self)
+        path?.addLineToPoint(p)
+        self.setNeedsDisplay()
+    }
+    
+    override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        self.touchesMoved(touches, withEvent: event)
+    }
+    
+    override func touchesCancelled(touches: Set<UITouch>?, withEvent event: UIEvent?) {
+        self.touchesEnded(touches!, withEvent: event)
+    }    
+}
